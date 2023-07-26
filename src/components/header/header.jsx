acd5f5e80b0d2ecc97d05e8600d7d536bb0e4909
danishhusain1611 <danishhusain2002@gmail.com>
2023-07-26 17:57:06 +0530

    Sixth commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi"; //search icon
+import { SlMenu } from "react-icons/sl"; //hamburger menu icon
+import { VscChromeClose } from "react-icons/vsc"; //close icon
+import { useNavigate, useLocation } from "react-router-dom"; //useNavigate method is imported for redirecting purpose.
 
 import "./style.scss";
 
-const Header = () => {
-  return (
-    <div>header</div>
-  )
-}
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
 
-export default Header
+const Header = () => {
+  const [show, setShow] = useState("top"); //this state will be used for creating scrolling effect.
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate(); //instance of useNavigate method.
+  const location = useLocation();
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""}`}> 
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">T.V. Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+
+        <div className="mobileMenuItems">
+          <HiOutlineSearch />
+          {
+            mobileMenu ? (
+              <VscChromeClose onClick={() => setMobileMenu(true)} />
+            ) : (
+              <SlMenu onClick={openMobileMenu} />
+            ) //if mobileMenu state will be true, it will show the close button. If not then it will show hamburgen menu icon.
+          }
+        </div>
+      </ContentWrapper>
+    </header>
+  );
+};
+
+export default Header;
