ea0de17d059281d1475eeaf895d60c67d4b4cc47
danishhusain1611 <danishhusain2002@gmail.com>
2023-07-28 21:53:08 +0530

    Completed Main Carousel Section

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,85 @@
+import React, { useRef } from "react";
+import {
+    BsFillArrowLeftCircleFill, //left icon in the carousel section to navigate left and right.
+    BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs"; //the release date date from the API is in a different format. With the help of dayjs we will convert that date data according to our suitable format.
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+
+import "./style.scss";
+
+const Carousel = ({ data, loading }) => {
+    const carouselContainer = useRef(); //wherever we pass this variable "carouselContainer" we will get that div reference.
+    const {url} = useSelector((state) => state.home);
+    const navigate = useNavigate();
+
+    const navigation = (dir) => {};
+
+    const skItem = () => {
+        return (
+            <div className="skeletonItem">
+                <div className="posterBlock skeleton"></div>
+                <div className="textBlock">
+                    <div className="title skeleton"></div>
+                    <div className="date skeleton"></div>
+                </div>
+            </div>
+        )
+    }
+    
+
+  return <div className="carousel">
+    <ContentWrapper>
+        <BsFillArrowLeftCircleFill 
+            className="carouselLeftNav arrow"
+            onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+            className="carouselRightNav arrow"
+            onClick={() => navigation("right")}
+        />
+        {!loading ? (
+            <div className="carouselItems">
+                {data?.map((item) => {
+                    const posterUrl = item.poster_path  //as URL is too long we passed item.poster_path in posterURL and then passed it into posterBlock.
+                    ? url.poster + item.poster_path 
+                    : PosterFallback; //if image in the movie card doesn't appear we passed this condtion so PosterFallback image will appear.
+                    return (
+                        <div 
+                        key={item.id} //each movie card in the carousel section have a differnt id which we have passed here.
+                        className="carouselItem">
+                            <div className="posterBlock">
+                                <Img src={posterUrl} />
+                            </div>
+                            <div className="textBlock">
+                                <span className="title">
+                                    {item.title || item.name} 
+                                </span>
+                                <span className="date">
+                                    {dayjs(item.release_Date).format
+                                    ("MMM D, YYYY")} 
+                                </span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        ) : (
+            <div className="loadingSkeleton">
+                {skItem()}
+                {skItem()}
+                {skItem()}
+                {skItem()}
+                {skItem()}
+            </div>
+        )}
+    </ContentWrapper>
+  </div>; //if i want this div reference i will use "ref" tag to take its reference.
+};
+
+export default Carousel
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -6,6 +6,7 @@ import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
 import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
 const Trending = () => {
     const [endpoint, setEndpoint] = useState("day");
@@ -23,6 +24,7 @@ const Trending = () => {
         <SwitchTabs data={["Day", "Week"]} onTabChange=
         {onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
